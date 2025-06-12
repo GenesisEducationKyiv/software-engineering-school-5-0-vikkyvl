1d3e27ff9d78922ba21169780e74f6bd90699e11
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-06-12 18:04:44 +0300

    Add padding-line-between-statements rule to ESLint configuration

--- api-gateway/eslint.config.mjs
+++ api-gateway/eslint.config.mjs
@@ -27,6 +27,10 @@ export default tseslint.config(
   },
   {
     rules: {
+      'padding-line-between-statements': [
+          'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: '*', next: 'throw' }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-unsafe-argument': 'warn'
