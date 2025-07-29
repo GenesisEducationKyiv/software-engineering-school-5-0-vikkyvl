c1a24cf307336f6c4b411dff553ab534d5f12ebf
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-07-29 08:50:52 +0300

    Add NotificationDto, fix test

--- platform-app/common/config/services/notification-config.service.ts
+++ platform-app/common/config/services/notification-config.service.ts
@@ -1,9 +1,8 @@
 import { BrokerConfigInterface } from './interfaces/broker-config.interface';
 import { ClientConfigInterface } from './interfaces/client-config.interface';
-import { EmailConfigInterface } from './interfaces/email-config.interface';
 
 class NotificationConfigService
-  implements BrokerConfigInterface, ClientConfigInterface, EmailConfigInterface
+  implements BrokerConfigInterface, ClientConfigInterface
 {
   public getBrokerUrl() {
     return process.env.BROKER_URL ?? 'amqps://...';
@@ -19,14 +18,6 @@ class NotificationConfigService
 
   public getReactAppApiUrl() {
     return process.env.REACT_APP_API_URL;
-  }
-
-  public getEmailUser() {
-    return process.env.EMAIL_USER;
-  }
-
-  public getEmailPassword() {
-    return process.env.EMAIL_PASSWORD;
   }
 }
 
